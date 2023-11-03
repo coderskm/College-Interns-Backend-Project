8eb00e03517c3d1bf7a1a53a90a0a3b0afd08b6c
Sumit Kumar Mishra <sumitkumarmishra.code@gmail.com>
2023-11-03 16:41:50 +0530

    done

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 const route = require("./routes/route.js");
 const app = express();
 
 app.use(express.static('./public'));
-app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("mongodb is connected 💯✅"))
+  .then(() => console.log("mongodb is connected"))
   .catch((err) => console.log(err));
 
 app.use("/", route);
